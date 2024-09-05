e3a864b08d5e8c94b8c9b946ab7389419ae71fc8
ambiencelife22 <118952328+ambiencelife22@users.noreply.github.com>
2024-09-05 20:47:45 +0000

    updates

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Sandbox</h1>
+      <p>Hi!</p>
+    </div>
+  )
+}
+
+export default Home;
